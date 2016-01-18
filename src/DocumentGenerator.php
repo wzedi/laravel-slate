f90c43fc680ed9fa843338d60b60799b0c754dc7
Warrick Zedi <warrick@loyaltycorp.com.au>
2016-01-18 12:47:00 +1100

    Simple layout generated to debug log

--- src/DocumentGenerator.php
+++ src/DocumentGenerator.php
@@ -2,8 +2,10 @@
 
 namespace CadreWorks\LaravelSlate;
 
-use Illuminate\Htt\Request;
-use Illuminate\Htt\Response;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Closure;
+use Log;
 
 class DocumentGenerator
 {
@@ -11,10 +13,80 @@ class DocumentGenerator
      * Generate tripit/slate documentation for a specific API request.
      *
      * @param Illuminate\Http\Request $request The request to document
-     * @param Illuminate\Http\Response $response An example response
+     * @param 
      */
-    static public function generate(Request $request, Response $response)
+    public function handle($request, Closure $next)
     {
+        $response = $next($request);
 
+        $method = $request->method();
+        $path = $request->path();
+        $response_content = $response->getOriginalContent();
+
+        $username = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : "";
+        $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : "";
+        $auth = "";
+        if (!empty($username))
+        {
+            $auth = "\n-u $username:$password \\\n";
+        }
+
+        $params = "";
+        foreach ($request->input() as $name => $value)
+        {
+            if (is_array($value))
+            {
+                foreach ($value as $inner_name => $inner_value)
+                {
+                    $params .= '-d "' . $name . '[' . $inner_name . ']=' . $inner_value. "\" \\\n";
+                }
+            }
+            else
+            {
+                $params .= "-d \"$name=$value\" \\";
+            }
+        }
+
+        $slate_doc = <<<SLATE_DOC
+            ##<Route name> 
+
+            ```shell
+            curl /$path \ $auth
+                -X $method \
+                $params
+            ```
+
+            ```PHP
+            ```
+
+            ```Java
+            ```
+
+            ```Ruby
+            ```
+
+            ```Python
+            ```
+
+            ```Node
+            ```
+
+            > The above command returns JSON structured like this:
+
+            ```json
+            $response_content
+            ```
+            
+            <Description>
+
+            ### HTTP Request
+
+            `$method /$path`
+
+SLATE_DOC;
+
+        Log::debug($slate_doc);
+
+        return $response;
     }
 }
